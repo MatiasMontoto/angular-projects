b5a1eda8ee2449f9d8894a6831c33694be60c4c4
MatiasMontoto <montoto.matias@yahoo.com.ar>
2021-12-07 17:51:49 -0300

    added nebular ui

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,7 +2,11 @@ import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-square",
-  template: ` <button>{{ value }}</button> `,
+  template: `
+    <button nbButton status="info" *ngIf="!value">{{ value }}</button>
+    <button nbButton status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton status="warning" *ngIf="value == 'O'">{{ value }}</button>
+  `,
   styles: ["button { width: 100%; height: 100%; font-size: 5em !important; }"],
 })
 export class SquareComponent {
